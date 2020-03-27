3edec10526b1d57d0b52ebdadb9a619731cb0160
ayeniidris <61968571+ayeniidris@users.noreply.github.com>
2020-03-27 00:05:05 +0000

    First-Js-task

---
+++ index.js
@@ -0,0 +1,15 @@
+let name = "Ayeni Idris";
+ console.log(name + " Courses enrolled for , HTML,CSS,JS and C#");
+ const data = [];
+
+ for (let i=0; i<=200; i++) {
+
+   if (i%2===0) {
+
+     data.push(i);
+
+   }
+
+ }
+
+ console.log(data);
